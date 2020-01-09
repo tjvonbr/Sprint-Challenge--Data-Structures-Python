a101fc4a638bdce18e9dafc947db089a6a4152e8
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2020-01-08 23:49:15 -0600

    Completed sprint challenge for python algos and data structures

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,22 +1,42 @@
 from doubly_linked_list import DoublyLinkedList
 
+# Didn't attempt the stretch goal--everything should check out though
 
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        self.size = 0
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+          self.storage.add_to_tail(item)
+          self.current = self.storage.tail
+
+        if self.storage.length == self.capacity:
+          self.current.value = item
+          if self.current is self.storage.tail:
+            self.current = self.storage.head
+          else:
+            self.current = self.current.next
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
-        # TODO: Your code here
+        current = self.storage.head
+        while current:
+          list_buffer_contents.append(current.value)
+          current = current.next
 
         return list_buffer_contents
+
+    # Created a function to see if the ring buffer is full
+    def isFull(self):
+        return self.size == self.capacity
+
+buffer = RingBuffer(5)
+print(buffer.isFull())
 
 # ----------------Stretch Goal-------------------
 
